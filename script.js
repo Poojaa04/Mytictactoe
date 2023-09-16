d81aa34e337c33b4c10bffc912f850919d336a56
Poojaa04 <116179218+Poojaa04@users.noreply.github.com>
2023-09-16 20:11:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,171 @@
+console.log("Welcome to Tic Tac Toe")
+
+let music = new Audio("bgm1.mp3")
+
+let audioTurn = new Audio("ting.mp3")
+
+let gameover = new Audio("happycat.mp3")
+
+let turn = "X"
+
+let isgameover = false;
+
+
+
+// Function to change the turn
+
+const changeTurn = ()=>{
+
+    return turn === "X"? "0": "X"
+
+}
+
+let totalMoves = 0;
+
+
+const checkWin = () => {
+    let boxtext = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0, 1, 2, 5, 5, 0],
+
+        [3, 4, 5, 5, 15, 0],
+
+        [6, 7, 8, 5, 25, 0],
+
+        [0, 3, 6, -5, 15, 90],
+
+        [1, 4, 7, 5, 15, 90],
+
+        [2, 5, 8, 15, 15, 90],
+
+        [0, 4, 8, 5, 15, 45],
+
+        [2, 4, 6, 5, 15, 135],
+
+    ];
+
+    wins.forEach(e => {
+        if (
+            (boxtext[e[0]].innerText === boxtext[e[1]].innerText) &&
+            (boxtext[e[2]].innerText === boxtext[e[1]].innerText) &&
+            (boxtext[e[0]].innerText !== "")
+        ) {
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won";
+            isgameover = true;
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
+            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
+            document.querySelector(".line").style.width = "20vw";
+        }
+    });
+
+    // Check for a draw
+    totalMoves++;
+    if (totalMoves === 9 && !isgameover) {
+        document.querySelector('.info').innerText = "It's a draw! Reset and try again:)";
+        isgameover = true;
+    }
+};
+
+
+// Game Logic
+
+//music.play()
+
+let boxes = document.getElementsByClassName("box");
+
+Array.from(boxes).forEach(element =>{
+
+    let boxtext = element.querySelector('.boxtext');
+
+    element.addEventListener('click', ()=>{
+
+        if(boxtext.innerText === ''){
+
+            boxtext.innerText = turn;
+
+            turn = changeTurn();
+
+            audioTurn.play();
+
+            checkWin();
+
+            if (!isgameover){
+
+                document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
+
+            } 
+
+        }
+
+    })
+
+})
+
+
+
+// Add onclick listener to reset 
+reset.addEventListener('click', () => {
+    Array.from(boxes).forEach(element => {
+        let boxtext = element.querySelector('.boxtext');
+        element.addEventListener('click', () => {
+            if (isgameover) {
+                isgameover=true
+                return;
+            }
+    
+         } )
+    }
+    )
+})
+
+// Add onclick listener to reset button
+reset.addEventListener('click', () => {
+    let boxtexts = document.querySelectorAll('.boxtext');
+    
+    Array.from(boxtexts).forEach(element => {
+        element.innerText = "";
+    });
+
+    turn = "X";
+    isgameover = false; // Reset isgameover to false
+    totalMoves = 0; // Reset the total moves
+    document.querySelector(".line").style.width = "0vw";
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px";
+    
+    // Reset the information display
+    document.querySelector('.info').innerText = "Turn for " + turn;
+    
+    // Re-enable click event listeners on boxes
+    Array.from(boxes).forEach(element => {
+        element.addEventListener('click', handleBoxClick);
+    });
+});
+
+// Function to handle box click
+const handleBoxClick = (event) => {
+    if (isgameover) {
+        // If the game is already over, return early and don't allow further moves.
+        return;
+    }
+
+    let boxtext = event.target.querySelector('.boxtext');
+
+    if (boxtext.innerText === '') {
+        boxtext.innerText = turn;
+        turn = changeTurn();
+        audioTurn.play();
+        checkWin();
+
+        if (!isgameover) {
+            document.querySelector('.info').innerText = "Turn for " + turn;
+        }
+    }
+};
+
+// Attach click event listener to boxes
+Array.from(boxes).forEach(element => {
+    element.addEventListener('click', handleBoxClick);
+});
+
+    
+
